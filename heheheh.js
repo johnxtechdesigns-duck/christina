cf45970b652c894e586ce2ec0dbc279c1ae17290
johnxtechdesigns-duck <johnxtechdesigns@gmail.com>
2026-01-10 23:58:01 +0530

    Create heheheh.js

---
+++ heheheh.js
@@ -0,0 +1,12 @@
+var pics=[ 
+      "cris.jpg",
+      "suprise.PNG",
+]
+
+var btn = document.querySelector("button");
+var img = document.querySelector("img");
+btn.addEventListener("click", function(){
+  
+   img.src = "cris.jpg";
+});
+
